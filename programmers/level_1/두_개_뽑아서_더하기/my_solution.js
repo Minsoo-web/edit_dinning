6b943515c76e694b0627a2ca8a9baba79be3b290
minsoo-web <zlemzlem5656@naver.com>
2021-01-04 15:53:09 +0900

    [추가] 두 개 뽑아서 더하기 마무리

--- programmers/level_1/두_개_뽑아서_더하기/my_solution.js
+++ programmers/level_1/두_개_뽑아서_더하기/my_solution.js
@@ -1,15 +1,14 @@
 function solution(numbers) {
-    const length = numbers.length;
-    let obj ={};
-    
-    for (let i =0; i< length; i++){
-        let last_number =numbers.pop();
-        // numbers = [2,1,3,4]
-        numbers.forEach(num =>{
-            let plus_number = num+last_number;
-            if(!obj[plus_number]) obj[plus_number] = plus_number;
-        })
-    }
+  const length = numbers.length;
+  let obj = {};
 
-    return Object.values(obj);
+  for (let i = 0; i < length; i++) {
+    let last_number = numbers.pop();
+    numbers.forEach(num => {
+      let plus_number = num + last_number;
+      if (!obj[plus_number]) obj[plus_number] = plus_number;
+    });
+  }
+
+  return Object.values(obj);
 }
